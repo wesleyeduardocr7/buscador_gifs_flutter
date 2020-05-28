84d4dcd0db9afae2709b1410be19fd2441c6cc04
wesleyeduardocr7 <wesley.eduardo@acad.ifma.edu.br>
2020-05-28 08:41:28 -0300

    iniciando_layout

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+
+import 'package:contador_gifs_flutter/ui/HomePage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "Buscador de Gifs",
+    home: HomePage(),
+  ));
+}
+
+
